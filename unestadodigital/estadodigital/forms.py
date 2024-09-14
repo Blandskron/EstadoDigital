553796c8d129789222f7a298a21ee80903591327
blandskron <bastianlandskronfreelancer@gmail.com>
2024-09-14 19:16:11 -0300

    update

--- unestadodigital/estadodigital/forms.py
+++ unestadodigital/estadodigital/forms.py
@@ -1,3 +1,4 @@
+# forms.py
 from django import forms
 from .models import Contact
 
@@ -18,9 +19,39 @@ class ContactForm(forms.ModelForm):
             'phone_number',
             'company',
             'region',
-            'track_of_interest',  # Asegúrate de incluir este campo
+            'track_of_interest',
             'data_sharing_consent',
         ]
         widgets = {
-            'track_of_interest': forms.Select(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Nombre',
+                'class': 'form-control',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Apellido',
+                'class': 'form-control',
+            }),
+            'rut': forms.TextInput(attrs={
+                'placeholder': 'RUT',
+                'class': 'form-control',
+            }),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'Correo electrónico',
+                'class': 'form-control',
+            }),
+            'phone_number': forms.TextInput(attrs={
+                'placeholder': 'Teléfono de contacto',
+                'class': 'form-control',
+            }),
+            'company': forms.TextInput(attrs={
+                'placeholder': 'Institución a la que representa',
+                'class': 'form-control',
+            }),
+            'region': forms.TextInput(attrs={
+                'placeholder': 'Región',
+                'class': 'form-control',
+            }),
+            'track_of_interest': forms.Select(attrs={
+                'class': 'form-control',
+            }),
         }
